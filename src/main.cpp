22333467d48343a9c2b87f48f664f0631c139248
林剑 <fideojlin@gmail.com>
2018-05-22 20:25:49 +0800

    添加多个server的示例

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 #include <cstring>
 #include <string>
 
+class HelloBusiness : public Business {
+  void OnRead(const Buffer &recvBuf, Buffer &sendBuf) const {
+    strncpy(sendBuf.base, "Hello world\n", 12);
+    sendBuf.base[12] = '\0';
+    sendBuf.len = 12;
+  }
+};
+
 class EchoBusiness : public Business {
   void OnRead(const Buffer &recvBuf, Buffer &sendBuf) const {
     size_t nread = recvBuf.len;
@@ -16,11 +24,13 @@ class EchoBusiness : public Business {
 
 int main() {
   std::string hostIp = "0.0.0.0";
+  HelloBusiness hello;
   EchoBusiness echo;
-  TCPServer server(hostIp, 7000, &echo);
+  TCPServer server1(hostIp, 7000, &hello), server2(hostIp, 7001, &echo);
   EventLoop el;
 
-  el.AddServer(&server);
+  el.AddServer(&server1);
+  el.AddServer(&server2);
   el.Run();
 
   return 0;
